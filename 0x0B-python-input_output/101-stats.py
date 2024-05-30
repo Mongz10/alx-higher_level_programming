1451f3bc49bd999f050983baf1e462f293b19895
Mongz10 <m.t.sibande@gmail.com>
2024-05-30 11:16:45 +0000

    0x0B-python-input_output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,41 +1,32 @@
-#!/bin/bash
+#!/usr/bin/python3
+"""This module defines a program that parses logs"""
 
-# Initialize variables
-total_size=0
-declare -A status_counts
 
-# Function to print statistics
-print_statistics() {
-    echo "Total file size: File size: $total_size"
-    for code in 200 301 400 401 403 404 405 500; do
-        count=${status_counts[$code]}
-        if [ -n "$count" ]; then
-            echo "$code: $count"
-        fi
-    done
-}
-
-# Trap Ctrl+C to print statistics
-trap 'print_statistics' INT
-
-# Read input line by line
-while read -r line; do
-    # Extract file size and status code
-    file_size=$(echo "$line" | awk '{print $NF}')
-    status_code=$(echo "$line" | awk '{print $(NF-1)}')
-
-    # Update total size
-    total_size=$((total_size + file_size))
-
-    # Update status code count
-    ((status_counts[$status_code]++))
-
-    # Print statistics every 10 lines
-    if ((++line_count % 10 == 0)); then
-        print_statistics
-    fi
-done
-
-# Print final statistics
-print_statistics
+import sys
+codes = ['200', '301', '400', '401',
+         '403', '404', '405', '500']
+code_hist = []
+total_size = 0
+i = 0
+try:
+    for line in sys.stdin:
+        # if line == '':
+        # sys.exit()
+        tokens = line.split(' ')
+        total_size += int(tokens[-1])
+        code_hist.append(tokens[-2])
+        if i == 9:
+            i = 0
+            print('File size: ' + str(total_size))
+            for code in codes:
+                if code in code_hist:
+                    print('{}: {}'.format(code, code_hist.count(code)))
+        else:
+            i += 1
+except KeyboardInterrupt:
+    print('File size: ' + str(total_size))
+    for code in codes:
+        if code in code_hist:
+            print('{}: {}'.format(code, code_hist.count(code)))
+    sys.exit()
 
